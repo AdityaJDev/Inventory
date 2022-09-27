e20d3c4f44b11176db85da08a6fa226107555098
AlterEgoAdj <asjoshi3198@gmail.com>
2022-09-27 11:58:04 +0530

    1st draft

--- management/forms.py
+++ management/forms.py
@@ -3,18 +3,6 @@ from django.forms import ModelForm
 from django.contrib.auth.models import User
 
 
-class AddAssetForm(ModelForm):
-    class Meta:
-        model = Asset
-        fields = "__all__"
-
-
-class AddUserForm(ModelForm):
-    class Meta:
-        model = User
-        fields = "__all__"
-
-
 class AddAccessoryForm(ModelForm):
     class Meta:
         model = Accessory
